868705dac2bc1fc0d0e674f460cdaaf32111d707
Stranoalfio98 <162742642+Stranoalfio98@users.noreply.github.com>
2026-08-16 14:45:32 +0200

    Delete src directory

--- src/pages/InserimentoDati.jsx
+++
@@ -1,8 +0,0 @@
-export default function InserimentoDati() {
-  return (
-    <div>
-      <h1>Inserimento Dati</h1>
-      <p>Qui andrà il form per registrare entrate, uscite e giroconti.</p>
-    </div>
-  );
-}
