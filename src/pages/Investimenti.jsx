a9afbcd51cdca613e0145bd9cedbdfdd6113b62f
Stranoalfio98 <162742642+Stranoalfio98@users.noreply.github.com>
2026-08-16 14:12:44 +0200

    Delete src directory

--- src/pages/Investimenti.jsx
+++
@@ -1,8 +0,0 @@
-export default function Investimenti() {
-  return (
-    <div>
-      <h1>Investimenti</h1>
-      <p>Qui andranno ETF, PAC, obiettivi (Norvegia 2027, Fondo Emergenza, ecc.) e BFP.</p>
-    </div>
-  );
-}
